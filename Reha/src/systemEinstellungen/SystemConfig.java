9ae84a3990ee53895a209c3a64ec566aa82952e0
Juergen Steinhilber <steinhilber@users.sourceforge.net>
2009-05-28 12:37:32 +0000

    *** empty log message ***

--- Reha/src/systemEinstellungen/SystemConfig.java
+++ Reha/src/systemEinstellungen/SystemConfig.java
@@ -560,7 +560,12 @@ public class SystemConfig {
 		lAdrPDaten = Arrays.asList(new String[]{"<PAdr1>","<PAdr2>","<PAdr3>","<PAdr4>","<PAdr5>","<PGeboren>","<PNname>","<PVname>","<PGeboren>"});
 
 		hmAdrRDaten = new HashMap<String,String>();
-		lAdrRDaten = Arrays.asList(new String[]{"<RAdr1>","<RAdr2>","<RAdr3>","<RAdr4>"});
+		lAdrRDaten = Arrays.asList(new String[]{"<Rnummer>","<Rdatum>","<Rposition1>","<Rposition2>","<Rposition3>"
+				,"<Rposition4>","<Rpreise1>","<Rpreise2>","<Rpreise3>","<Rpreise4>","<Rproz1>","<Rproz2>","<Rproz3>"
+				,"<Rproz4>","<Rgesamt1>","<Rgesamt2>","<Rgesamt3>","<Rgesamt4>","<Rpauschale>","<Rendbetrag"});
+		for(int i = 0; i < lAdrRDaten.size(); i++){
+			hmAdrRDaten.put(lAdrRDaten.get(i),"");
+		}
 		
 	}
 	public static void DesktopLesen(){
