c76b043828e553179c67333abf3f21bad0b92d81
Juergen Steinhilber <steinhilber@users.sourceforge.net>
2009-08-11 15:28:00 +0000

    *** empty log message ***

--- Reha/src/systemEinstellungen/SysUtilFremdprogramme.java
+++ Reha/src/systemEinstellungen/SysUtilFremdprogramme.java
@@ -1,27 +1,38 @@
 package systemEinstellungen;
 
+import hauptFenster.Reha;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.LinearGradientPaint;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.Point2D;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.table.DefaultTableModel;
 
 import org.jdesktop.swingx.JXPanel;
 import org.jdesktop.swingx.JXTable;
 import org.jdesktop.swingx.painter.CompoundPainter;
 import org.jdesktop.swingx.painter.MattePainter;
+
+import patientenFenster.Dokumentation;
+import patientenFenster.MyAccessory;
 
 import systemEinstellungen.SysUtilKrankenkasse.MyVorlagenTableModel;
 import systemEinstellungen.SysUtilKrankenkasse.TitelEditor;
@@ -44,6 +55,7 @@ public class SysUtilFremdprogramme extends JXPanel implements KeyListener, Actio
 	JTextField oopfad = null;
 	JTextField adobepfad = null;
 	JTextField grafpfad = null;
+	String lpfad = null;
 	
 	
 	public SysUtilFremdprogramme(){
@@ -63,7 +75,8 @@ public class SysUtilFremdprogramme extends JXPanel implements KeyListener, Actio
 		/****/
 	     add(getVorlagenSeite(),BorderLayout.CENTER);
 	     add(getKnopfPanel(),BorderLayout.SOUTH);
-		return;
+	     validate();
+	     return;
 	}
 	
 	
@@ -94,7 +107,7 @@ public class SysUtilFremdprogramme extends JXPanel implements KeyListener, Actio
 			jpan.add(speichern, jpancc.xy(5,3));
 			jpan.addLabel("Änderungen übernehmen?", jpancc.xy(1,3));
 			
-			
+			jpan.getPanel().validate();
 			return jpan.getPanel();
 		}
 	
@@ -123,8 +136,12 @@ public class SysUtilFremdprogramme extends JXPanel implements KeyListener, Actio
 		adobepfad = new JTextField();
 		grafpfad = new JTextField();
 		
+		oopfad.setEditable(false);
+		adobepfad.setEditable(false);
+		grafpfad.setEditable(false);
+		//FormLayout lay = new FormLayout("right:max(80dlu;p), 4dlu, 180dlu, 4dlu, 60dlu",				
         //                                      1.            2.    3.    4.     5.     6.    7.      8.     9.
-		FormLayout lay = new FormLayout("right:max(80dlu;p), 4dlu, 180dlu, 4dlu, 60dlu",
+		FormLayout lay = new FormLayout("right:max(80dlu;p), 4dlu, 160dlu, 4dlu, 60dlu",
        //1.    2.      3.   4.   5. 6.   7.   8. 9.    10.  11. 12. 13.  14.  15. 16.  17. 18.  19.   20.    21.   22.   23.
 		"120dlu, 2dlu,p,2dlu, p, 10dlu, p, 10dlu, p, 2dlu, p, 2dlu,p, 10dlu");
 		
@@ -138,6 +155,7 @@ public class SysUtilFremdprogramme extends JXPanel implements KeyListener, Actio
 		//progtab.getColumn(0).setCellEditor(new TitelEditor());
 		progtab.setSortable(false);
 		JScrollPane jscrProg = JCompTools.getTransparentScrollPane(progtab);
+		jscrProg.validate();
 		builder.add(jscrProg, cc.xyw(1,1,5));
 		
 		builder.addLabel("markiertes Programm aus Liste entfernen", cc.xyw(1,3,3, CellConstraints.RIGHT, CellConstraints.BOTTOM));
@@ -148,13 +166,13 @@ public class SysUtilFremdprogramme extends JXPanel implements KeyListener, Actio
 		builder.addLabel("Pfad zu OpenOffice", cc.xy(1, 9));
 		builder.add(oopfad, cc.xy(3, 9));
 		builder.add(button[2], cc.xy(5, 9));
-		builder.addLabel("Pfad zu AdobeReader", cc.xy(1,11));
+		builder.addLabel("AdobeReader auswählen", cc.xy(1,11));
 		builder.add(adobepfad, cc.xy(3, 11));
 		builder.add(button[3],cc.xy(5, 11));
-		builder.addLabel("Graphikbearbeitung", cc.xy(1,13));
+		builder.addLabel("Graphikbearbeitung auswählen", cc.xy(1,13));
 		builder.add(grafpfad, cc.xy(3, 13));
 		builder.add(button[4],cc.xy(5, 13));
-		
+		builder.getPanel().validate();
 		return builder.getPanel();
 	}
 
@@ -179,9 +197,71 @@ public class SysUtilFremdprogramme extends JXPanel implements KeyListener, Actio
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		String cmd = e.getActionCommand();
+		if(cmd.equals("oopfad")){
+        	setCursor(new Cursor(Cursor.WAIT_CURSOR));
+			String pfad = progWaehlen(0);
+			if(!pfad.equals("")){
+				oopfad.setText(pfad);
+			}
+		}
+		if(cmd.equals("adobepfad")){
+			String pfad = progWaehlen(1);
+			if(!pfad.equals("")){
+				adobepfad.setText(pfad);
+			}
+		}
+		if(cmd.equals("grafpfad")){
+			String pfad = progWaehlen(1);
+			if(!pfad.equals("")){
+				grafpfad.setText(pfad);
+			}
+		}
 	}
 
+	public String progWaehlen(int welchesProg){
+	String sret = "";	
+	JFileChooser chooser = new JFileChooser("Verzeichnis wÃhlen");
+    chooser.setDialogType(JFileChooser.OPEN_DIALOG);
+    chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+    //
+    if(lpfad == null){
+    	lpfad = Reha.proghome;
+    }
+   	File file = new File(lpfad);	
+    chooser.setCurrentDirectory(file);
+    chooser.addPropertyChangeListener(new PropertyChangeListener() {
+        public void propertyChange(PropertyChangeEvent e) {
+            if (e.getPropertyName().equals(JFileChooser.SELECTED_FILE_CHANGED_PROPERTY)
+                    || e.getPropertyName().equals(JFileChooser.DIRECTORY_CHANGED_PROPERTY)) {
+                final File f = (File) e.getNewValue();
+            }
+        }
+    });
+    chooser.setVisible(true);
+    setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+    final int result = chooser.showOpenDialog(null);
+
+    if (result == JFileChooser.APPROVE_OPTION) {
+        File inputVerzFile = chooser.getSelectedFile();
+        String inputVerzStr = inputVerzFile.getPath();
+        lpfad = inputVerzStr;
+
+        if(inputVerzFile.getName().trim().equals("")){
+        	sret = inputVerzStr;
+        }else{
+        	sret = inputVerzFile.getAbsolutePath();
+        }
+    }else{
+    	sret = "";
+    }
+    chooser.setVisible(false); 
+    chooser.removeAll();
+    chooser = null;
+    return sret;
+	}
+	
+	
 }
 class MyProgTableModel extends DefaultTableModel{
 	   /**
