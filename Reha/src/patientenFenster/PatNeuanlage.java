bab98d170a3fd9edaef76f2048ffaff8f609a7e5
Juergen Steinhilber <steinhilber@users.sourceforge.net>
2009-08-16 15:36:54 +0000

    *** empty log message ***

--- Reha/src/patientenFenster/PatNeuanlage.java
+++ Reha/src/patientenFenster/PatNeuanlage.java
@@ -785,10 +785,10 @@ boolean inNeu = false;
 		
 									//      1.                2.    3.     4.                 5.    6.    
 		FormLayout lay = new FormLayout("right:max(80dlu;p), 4dlu, 60dlu,right:max(60dlu;p), 4dlu, 60dlu",
-			       //1.   2.  3.   4.   5.   6  7   8    9   10   11  12  13  14   15   16  17  18   19   20   21  22   23  24   25   26   27  28  29   30   31   32  33  34    35  36   37  38   39    40   41   42  43   44   45  46  47  48    49   50   51 52   53  54    55  56  57   58   59   60   61   62   63  64   65    66    67
-					"p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 10dlu, p, 10dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 10dlu, p, 10dlu, p, 2dlu, p, 2dlu, p, 10dlu, p, 10dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 10dlu, p , 10dlu, p, 2dlu, p, 2dlu, p, 10dlu, p, 10dlu, p,"+
-				   //68   69  70   71   72   73   74  75 76    77  78   79 71 72
-					"2dlu, p, 2dlu, p, 2dlu, p,  2dlu, p, 2dlu, p, 10dlu,p,10dlu");
+			       //1.   2.  3.   4.   5.   6  7   8    9   10   11  12  13  14   15   16  17  18   19   20   21  22   23  24   25  26   27  28  29   30   31   32  33  34   35  36   37  38   39    40  41  42  43   44   45  46  47  48    49   50   51 52   53  54   55  56  57   58   59   60  61   62  63  64   65   66   67
+					"p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 7dlu, p, 7dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 7dlu, p, 7dlu, p, 2dlu, p, 2dlu, p,  7dlu, p, 7dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 7dlu, p , 7dlu, p, 2dlu, p, 2dlu, p, 7dlu, p, 7dlu, p,"+
+				   //68   69  70   71   72  73   74   75   76  77  78  79 80 
+					"2dlu, p, 2dlu, p, 2dlu, p,  2dlu, p, 2dlu, p, 7dlu,p,7dlu");
 					PanelBuilder builder = new PanelBuilder(lay);
 					builder.setDefaultDialogBorder();
 					builder.getPanel().setOpaque(false);	
