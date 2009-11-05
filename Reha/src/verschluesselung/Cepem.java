79039c0c3b6feced49b6814622abc669eb222daa
Juergen Steinhilber <steinhilber@users.sourceforge.net>
2009-11-05 16:00:28 +0000

    *** empty log message ***

---
+++ Reha/src/verschluesselung/Cepem.java
@@ -0,0 +1,25 @@
+package verschluesselung;
+
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.win32.StdCallFunctionMapper;
+import com.sun.jna.win32.StdCallLibrary;
+
+public class Cepem {
+	//	sString := "-e -u"+"IK510841109"+" -P"+sCepemPW+" -O -Y* -S -r IK"+sCepemIK+" -i "+sEsolOrg+;
+	//" -o "+sEsolSls+" -ZZufallszeichen -k"
+	//L_Ret := CEPEM_Call(String2Psz(sString))
+	//_DLL FUNCTION CEPEM_Call(cmd AS PSZ) AS LONG PASCAL:CepemDLL._CEPEM_Call@4
+	interface CepemDLL extends StdCallLibrary { 
+
+		 StdCallFunctionMapper mapper = new StdCallFunctionMapper();
+
+		 CepemDLL INSTANCE = (CepemDLL) Native.loadLibrary("CEPEM", CepemDLL.class);
+
+		 public int Cepem_Call(
+				 String Cmd 
+		 		);
+
+	} 
+
+}
