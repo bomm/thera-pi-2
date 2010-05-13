9ae55c5d12eafad901fd6f6e34ed676ff3a444ec
Juergen Steinhilber <thera-pi@users.sourceforge.net>
2010-05-13 08:15:45 +0000

    *** empty log message ***

---
+++ Nebraska/src/utils/ButtonTools.java
@@ -0,0 +1,16 @@
+package utils;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class ButtonTools {
+	
+	public static JButton macheBut(String titel,String cmd,ActionListener al){
+		JButton but = new JButton(titel);
+		but.setActionCommand(cmd);
+		but.addActionListener(al);
+		return but;
+	}
+
+}
