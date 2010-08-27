fc11274ee0fff93c3dd197b428e70cfe8c5ca237
Juergen Steinhilber <thera-pi@users.sourceforge.net>
2010-08-27 15:12:41 +0000

    Tools für das Kassenhandling

---
+++ Reha/src/krankenKasse/KTraegerTools.java
@@ -0,0 +1,27 @@
+package krankenKasse;
+
+import java.util.Vector;
+
+import sqlTools.SqlInfo;
+
+public class KTraegerTools {
+
+	public static String getEmailAdresse(String ik){
+		return SqlInfo.holeEinzelFeld("select email from ktraeger where ikkasse='"+ik+"' LIMIT 1");
+	}
+	public static String getKtraegerIK(String ik){
+		return SqlInfo.holeEinzelFeld("select ikkostentraeger  from ktraeger where ikkasse='"+ik+"' LIMIT 1");
+	}
+	public static String getDatenIK(String ik){
+		return SqlInfo.holeEinzelFeld("select ikdaten  from ktraeger where ikkasse='"+ik+"' LIMIT 1");
+	}
+	public static String getNutzerIK(String ik){
+		return SqlInfo.holeEinzelFeld("select ikentschluesselung from ktraeger where ikkasse='"+ik+"' LIMIT 1");
+	}
+	public static String getPapierIK(String ik){
+		return SqlInfo.holeEinzelFeld("select ikpapier from ktraeger where ikkasse='"+ik+"' LIMIT 1");
+	}
+	public static Vector<Vector<String>> getPapierAdresse(String ik){
+		return SqlInfo.holeFelder("select name1,name2,name3,adresse1,adresse1,adresse1,email from ktraeger where ikkasse='"+ik+"' LIMIT 1");
+	}
+}
