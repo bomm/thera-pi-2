1329c1769aca2f00c726f9a05f7abadd29f912f3
Juergen Steinhilber <thera-pi@users.sourceforge.net>
2010-11-16 11:50:10 +0000

    Version angepaßt

--- Reha/src/hauptFenster/Reha.java
+++ Reha/src/hauptFenster/Reha.java
@@ -299,7 +299,7 @@ public class Reha implements FocusListener,ComponentListener,ContainerListener,M
 	public static boolean demoversion = false;
 	public static boolean vollbetrieb = true;
 
-	public static String aktuelleVersion = "V=2010-11-15/02-DB=";
+	public static String aktuelleVersion = "V=2010-11-16/01-DB=";
 	
 	public static Vector<Vector<Object>> timerVec = new Vector<Vector<Object>>();
 	public static Timer fangoTimer = null;
