f1acaf83139b6dcd8ab26512750b5e5795312a57
Juergen Steinhilber <thera-pi@users.sourceforge.net>
2011-01-19 08:54:55 +0000

    Quelltextpflege

--- Reha/src/patientenFenster/HistorDaten.java
+++ Reha/src/patientenFenster/HistorDaten.java
@@ -249,7 +249,8 @@ public class HistorDaten extends JXPanel{
 
 		
 	}
-	@SuppressWarnings("unchecked")
+	
+	@SuppressWarnings("rawtypes")
 	public String leistungTesten(int leistung,Vector<Vector<String>> preisevec,int veczahl){
 		String retwert = "----";
 		if(veczahl==-1 || veczahl==0){
