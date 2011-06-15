0bcfb142bd847e65f44f48fbe8f225a0a7ee8454
Juergen Steinhilber <thera-pi@users.sourceforge.net>
2011-06-15 06:24:58 +0000

    *** empty log message ***

--- RehaMail/src/RehaIO/RehaIOMessages.java
+++ RehaMail/src/RehaIO/RehaIOMessages.java
@@ -10,4 +10,6 @@ public class RehaIOMessages {
 	public static String MUST_DOKUFIND = "DOKUSUCHEN";
 	public static String MUST_OPFIND = "OPSUCHEN";
 	public static String MUST_GOTOFRONT = "TOFRONT";
+	public static String MUST_RESET = "RESET";
+	public static String MUST_SEARCHFORMAIL = "SEARCHMAIL";
 }
