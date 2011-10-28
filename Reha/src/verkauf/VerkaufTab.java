3c08411dff2d1f98d4c70778a1da273ddd948705
Juergen Steinhilber <thera-pi@users.sourceforge.net>
2011-10-28 13:34:08 +0000

    Rechtehandling für die Artikelseite

--- Reha/src/verkauf/VerkaufTab.java
+++ Reha/src/verkauf/VerkaufTab.java
@@ -3,16 +3,19 @@ package verkauf;
 import java.awt.BorderLayout;
 
 import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.jdesktop.swingx.JXPanel;
 
 import com.jgoodies.looks.windows.WindowsTabbedPaneUI;
 
+import rechteTools.Rechte;
 import rehaInternalFrame.JVerkaufInternal;
 
 
 
-public class VerkaufTab extends JXPanel {
+public class VerkaufTab extends JXPanel implements ChangeListener {
 
 
 	/**
@@ -33,8 +36,19 @@ public class VerkaufTab extends JXPanel {
 		JTabbedPane pane = new JTabbedPane();
 		//damit das Design gleich ist wie alle anderen TabbedPanes in Thera-Pi
 		pane.setUI(new WindowsTabbedPaneUI());
+		pane.addChangeListener(this);
 		pane.add("Verkäufe tätigen", new VerkaufGUI());
 		pane.add("Lagerverwaltung", new LagerGUI());	
 		return pane;
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		JTabbedPane pane = (JTabbedPane)e.getSource();
+		if(pane.getSelectedIndex() > 0 &&
+				Rechte.hatRecht(Rechte.Sonstiges_artikelanlegen, true)){
+			pane.setSelectedIndex(0);
+			return;
+		}
 	}
 }
