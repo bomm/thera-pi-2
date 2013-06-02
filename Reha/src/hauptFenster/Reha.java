948c87db1573ef8233624487d6a70bba13f74a76
Juergen Steinhilber <thera-pi@users.sourceforge.net>
2013-06-02 11:31:57 +0000

    *** empty log message ***

--- Reha/src/hauptFenster/Reha.java
+++ Reha/src/hauptFenster/Reha.java
@@ -312,7 +312,7 @@ public class Reha implements FocusListener,ComponentListener,ContainerListener,M
 	public static boolean demoversion = false;
 	public static boolean vollbetrieb = true;
 
-	public static String aktuelleVersion = "2013-05-22-DB=";
+	public static String aktuelleVersion = "2013-05-28-DB=";
 	
 	public static Vector<Vector<Object>> timerVec = new Vector<Vector<Object>>();
 	public static Timer fangoTimer = null;
